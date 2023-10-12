86153c62d7efd6823e0213f6d06a5fe209bd134f
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-10-12 14:56:50 +0300

    PortfolioPageChange

--- resources/views/page/portfolio/show/default.blade.php
+++ resources/views/page/portfolio/show/default.blade.php
@@ -36,15 +36,17 @@
         </div>
     </section>
     @endif
-    <section class="pb-12" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1000" data-aos-anchor-placement="top-bottom">
-		<div class="container">
-            <x-slider.slider class="swiper-photography gallery-slider">
-                @foreach (getImages($portfolio->images) as $image)
-                    <x-slider.slider-photography-item :item="$image" :entity="$portfolio" />
-                @endforeach
-            </x-slider.slider>
-        </div>
-    </section>
+    @if(! empty$portfolio->images))
+        <section class="pb-12" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1000" data-aos-anchor-placement="top-bottom">
+            <div class="container">
+                <x-slider.slider class="swiper-photography gallery-slider">
+                    @foreach (getImages($portfolio->images) as $image)
+                        <x-slider.slider-photography-item :item="$image" :entity="$portfolio" />
+                    @endforeach
+                </x-slider.slider>
+            </div>
+        </section>
+    @endif
     <section class="pb-12" data-aos="fade-up" data-aos-easing="linear" data-aos-duration="1000" data-aos-anchor-placement="top-bottom">
 		<div class="container">
             <x-pre-footer-action text="Хотите также?"/>
