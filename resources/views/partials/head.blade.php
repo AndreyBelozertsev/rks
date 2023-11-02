ae7bbbe62f328a3b911c2e5b47c1142048e004e5
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-11-02 13:50:23 +0300

    YandexMapPageRefresh

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -20,6 +20,7 @@
 	<link rel="mask-icon" href="./images/safari-pinned-tab.svg" color="#1E1F43">
 	<meta name="msapplication-TileColor" content="#1E1F43">
 	<meta name="theme-color" content="#1E1F43">
+	<meta name="yandex-verification" content="6e494df6f8b03c48" />
 	<!-- Yandex.Metrika counter --> 
 	<script type="text/javascript">(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date(); for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }} k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(48507413, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> 
 	<!-- /Yandex.Metrika counter -->
