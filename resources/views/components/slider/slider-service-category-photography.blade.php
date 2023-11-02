e8bdf86ffbe40be0ccce1b1e98edb56467f7e127
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-11-02 11:56:48 +0300

    SliderRefresh

--- resources/views/components/slider/slider-service-category-photography.blade.php
+++ resources/views/components/slider/slider-service-category-photography.blade.php
@@ -3,7 +3,7 @@
     'entity',
  ])
  <div class="swiper-slide">
-    <div class="rounded-lg overflow-hidden">
+    <div class="rounded-lg overflow-hidden flex justify-center">
         <div>
             <img src="{{ asset($entity->makeThumbnailFromImg('345x320', $item['url'], 'fit'))}}" alt="slide">
         </div>
